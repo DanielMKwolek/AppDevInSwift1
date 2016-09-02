d21ee16048da48461d91d8bc6271ac9a5896672a
Daniel Kwolek <Daniel@Daniels-MacBook-Pro.local>
2016-09-02 13:31:18 -0400

    Initial commit

---
+++ 03-Calculations.xcplaygroundpage/Contents.swift
@@ -0,0 +1,27 @@
+//: ## Calculations
+//: You can use a playground like a calculator. Type in the math problem you want solved and the results sidebar shows the answer, or the _result_, of the calculation.
+//:
+//: You can use the plus `+` and minus `-` signs for addition and subtraction just as you’d expect:
+34 + 56 + 230
+
+1000 - 300
+
+3+4+2+5+3+1+5+8+4+9+3+0
+
+//: For multiplication, use the asterisk `*` :
+3 * 100
+9*8*7*6*5*5*4*3*2*1
+//: For division, use the slash `/` :
+24 / 8
+
+5/9
+//: For grouping, use parenthesis `( )` :
+(300 + 500 + 400) / 3
+
+(5/9) * 5 * 4 * 2 * 29 * 100
+//: - experiment:\
+//: Practice using the playground like a calculator.
+//: Edit some of the existing expressions and a few of your own.
+//: Notice every time you make a change the results are updated in the sidebar.
+//:
+//:[Previous](@previous)  |  page 3 of 7  |  [Next: Comments](@next)
